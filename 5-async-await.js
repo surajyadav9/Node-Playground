f4ba54c34ebfa51daee7ae3ad846805a28c478ab
Surya <surajyadav90388@gmail.com>
2019-07-09 20:29:33 +0530

    added condition to reject the promise

--- 5-async-await.js
+++ 5-async-await.js
@@ -2,6 +2,10 @@
 const add = (a , b) => {
     return new Promise((resolve , reject) => {
         setTimeout(()=>{
+            if (a<0 || b<0) {
+                reject('numbers must be non-negative')
+            }
+
             resolve(a+b)
         } , 2000)
     })
@@ -10,7 +14,7 @@ const add = (a , b) => {
 
 
 const doWork = async () => {
-    const sum = await add(1, 99)
+    const sum = await add(1, -99)
     const sum2 = await add(sum , 50)
     const sum3 = await add(sum2 , 50)
 
